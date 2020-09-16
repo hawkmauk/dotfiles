519f31d8a1994dc5a8fd65f6c7f1355fb53e2181
Michael Hawkins <michael.hawkins@oneadvanced.com>
2020-09-16 09:13:30 +0100

    Add inputrc to remove bell!

--- install.sh
+++ install.sh
@@ -1,1 +1,15 @@
-ln -s $(pwd)/.vimrc ${HOME}/.vimrc 
+for dotfile in .vimrc .inputrc
+do
+        source=$(pwd)/${dotfile}
+        target=${HOME}/${dotfile}
+
+        # backup target if it exists
+	echo "checking for existing [${target}]"
+        if [ -f "${target}" ]; then
+		echo "backup existing"
+                mv ${target} ${target}_$(date +%s)
+        fi
+        
+        # create the link to the dotfile
+        ln -s ${source} ${target}
+done
